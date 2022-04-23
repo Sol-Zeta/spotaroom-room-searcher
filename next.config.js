fffbb10b8e28c2450ef28c9b34fbf3931e2c5dd0
Sol-Zeta <mspattoglio@gmail.com>
2022-04-23 17:06:59 +0200

    install libraries

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['d1052pu3rm1xk9.cloudfront.net'],
+  },
+  env: {
+    API_BASE_URL: 'https://www.spotahome.com/api/public/listings/search/'
+  },
 }
 
 module.exports = nextConfig
