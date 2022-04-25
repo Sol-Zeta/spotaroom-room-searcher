f59a3ad9d02c33a155468c78f7683232d77b5196
Sol-Zeta <mspattoglio@gmail.com>
2022-04-25 19:09:11 +0200

    fix Icon component

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import styles from './Icon.module.scss';
 
 interface Props {
   icon: IconProp;
 }
 
 export const Icon = ({ icon }: Props) => (
-  <div>
+  <div className={styles.container}>
     <FontAwesomeIcon icon={icon} />
   </div>
 );
