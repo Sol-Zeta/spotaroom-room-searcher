af6e9d701eb47b59415490afc23695ab27b7091b
Sol-Zeta <mspattoglio@gmail.com>
2022-04-25 15:24:52 +0200

    add icons

---
+++ src/assetsRoutes/icons.ts
@@ -0,0 +1,13 @@
+import { faArrowAltCircleUp, faArrowAltCircleDown, faArrowAltCircleLeft, faArrowAltCircleRight, faWindowClose, faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as faHeartSolid, faMagnifyingGlass}  from '@fortawesome/free-solid-svg-icons';
+
+export const icons = {
+    arrow_up: faArrowAltCircleUp,
+    arrow_down: faArrowAltCircleDown,
+    arrow_left: faArrowAltCircleLeft,
+    arrow_right: faArrowAltCircleRight,
+    cross: faWindowClose,
+    heart_line: faHeart,
+    heart_solid: faHeartSolid,
+    search: faMagnifyingGlass
+}
