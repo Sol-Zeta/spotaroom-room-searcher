71458e47478bda3ed8c0f64e212f4559520f819f
Sol-Zeta <mspattoglio@gmail.com>
2022-04-25 15:26:00 +0200

    fix Head component

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -7,6 +7,7 @@ export const Head = () => {
       <title>Spot a Room</title>
       <meta name="description" content="Find the room of your dreams" />
       <link rel="icon" href="/favicon.ico" />
+      {/* <script defer src="/src/assets/fontawesome/js/all.js"></script> */}
     </NextHead>
   );
 };
