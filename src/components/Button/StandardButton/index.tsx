75d7ab9d1cdd4d8c611555d54bd063a615d954b0
Sol-Zeta <mspattoglio@gmail.com>
2022-04-25 19:09:31 +0200

    add new prop to Button

--- src/components/Button/StandardButton/index.tsx
+++ src/components/Button/StandardButton/index.tsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./StandardButton.module.scss";
 
 interface Props {
   text: string;
+  customStyles?: any;
   secondary?: boolean;
   onClick: () => void;
 }
 
-export const StandardButton = ({ text, onClick, secondary }: Props) => {
+export const StandardButton = ({ text, customStyles, onClick, secondary }: Props) => {
+  
+  const [isClicked, setIsClicked] = useState(false)
+  
   return (
     <button
-      className={`${styles.container} ${
-        secondary && styles.secondary_container
-      }`}
+      className={`
+        ${styles.container} 
+        ${secondary && styles.secondary_container} 
+        ${isClicked && styles.clicked}
+        ${customStyles}
+      `}
       onClick={onClick}
+      onMouseDown={()=> setIsClicked(true)}
+      onMouseUpCapture={()=> setIsClicked(false)}
     >
       <p className={`${styles.text} ${secondary && styles.secondary_text}`}>
         {text}
