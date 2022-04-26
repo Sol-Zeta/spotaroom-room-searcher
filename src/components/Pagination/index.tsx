0dd242282ce3d59bdda00962cb428d2bae2b69ef
Sol-Zeta <mspattoglio@gmail.com>
2022-04-26 10:17:36 +0200

    create Pagination component

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+
+
+interface Props {
+    totalPages: number;
+    activePage: number;
+    onChange: (value: number) => void;
+}
+
+export const PaginationComponent = ({totalPages, activePage, onChange}: Props) => {
+  console.log("pagination", totalPages, activePage)
+  const handleChange = (event: any, value: number) => {
+    onChange(value);
+  };
+
+  return (
+    <Stack spacing={2}>
+      <Pagination count={totalPages} page={activePage} onChange={handleChange} />
+    </Stack>
+  );
+}
