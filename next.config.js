626ef3208f3787c8aeb0e83a717382b3240d1d75
Sol-Zeta <mspattoglio@gmail.com>
2022-04-26 18:50:26 +0200

    add router query params funcion

--- next.config.js
+++ next.config.js
@@ -1,19 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextTranslate = require("next-translate");
-const path = require('path')
+const path = require("path");
 
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['d1052pu3rm1xk9.cloudfront.net'],
+    domains: ["d1052pu3rm1xk9.cloudfront.net"],
   },
   env: {
-    API_BASE_URL: 'https://www.spotahome.com/api/public/listings/search/'
+    API_BASE_URL: "https://www.spotahome.com/api/public/listings/search/",
   },
   sassOptions: {
-    includePaths: [path.join(__dirname, '.'), path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "."), path.join(__dirname, "styles")],
   },
-  ...nextTranslate()
-}
+  async rewrites() {
+    return [
+      { source: "/:city*", destination: "/:city*" },
+      { source: "/", destination: "/madrid" },
+    ];
+  },
+  ...nextTranslate(),
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
