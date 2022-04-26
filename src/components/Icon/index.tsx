fc0c8d222ef07bcc62e7b0f90e5d70f7c6322a2b
Sol-Zeta <mspattoglio@gmail.com>
2022-04-26 14:11:49 +0200

    add light property

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -5,10 +5,11 @@ import styles from './Icon.module.scss';
 
 interface Props {
   icon: IconProp;
+  light?: boolean;
 }
 
-export const Icon = ({ icon }: Props) => (
+export const Icon = ({ icon, light }: Props) => (
   <div className={styles.container}>
-    <FontAwesomeIcon icon={icon} />
+    <FontAwesomeIcon icon={icon} color={light ? '#E6E4E5' : '#131213'}/>
   </div>
 );
