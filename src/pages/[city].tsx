9ee503e454a858ccc8bdb8097e37a779fc1aa676
Sol-Zeta <mspattoglio@gmail.com>
2022-04-26 18:50:15 +0200

    add router query params funcion

--- src/pages/[city].tsx
+++ src/pages/[city].tsx
@@ -1,18 +1,18 @@
 import { useState, useEffect, useContext, useRef } from "react";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
 import useTranslation from "next-translate/useTranslation";
 import { PropertiesContext } from "../context";
 import {
   IconButton,
   Head,
-  Footer,
   AutoCompleteInput,
   SelectInput,
   CardList,
   Pagination,
   StandardButton,
 } from "../components";
-import { createFileName, createHref, downloadFile, capitalizeWords } from "../utils";
+import { createFileName, downloadFile, capitalizeWords } from "../utils";
 import { cityOptions, propertiesTypesOptions } from "../data";
 
 import styles from "../../styles/Home.module.scss";
@@ -21,7 +21,6 @@ import { CheckboxInput } from "../components/FormInputs/CheckboxInput";
 const Home: NextPage = () => {
   const {
     isContextReady,
-    isLoadingProperties,
     properties,
     totalProperties,
     priceOrder,
@@ -33,55 +32,55 @@ const Home: NextPage = () => {
     page,
     setPage,
     itemsPerPage,
-    setItemsPerPage,
   } = useContext(PropertiesContext);
-  const [propertiesList, setPropertiesList] = useState<any>([]);
   const [city, setCity] = useState(cityFilter);
-  const [types, setTypes] = useState([]);
-  const [fileToDownload, setFileToDownload] = useState("");
-  const [href, setHref] = useState("");
-
   const { t } = useTranslation();
+
+  const router = useRouter();
 
   let downloadLinkRef = useRef<HTMLAnchorElement>(null);
 
   useEffect(() => {
-    if (properties) {
-      setPropertiesList(properties);
+    console.log("ROUTER ************", router.query);
+    if (router.query.city) {
+      setCity(router.query.city);
+      setCityFilter(router.query.city);
     }
-  }, [properties]);
+  }, []);
 
   useEffect(() => {
-    console.log("CONTEXT READY", isContextReady);
-  }, [isContextReady]);
+    console.log("CITY FILTER", cityFilter);
+  }, [cityFilter]);
 
   const translateCities = (options: string[]) =>
     options.map((e: string) => ({
-      label: capitalizeWords(e ? t(`cities:${e}`) : ''),
+      label: capitalizeWords(e ? t(`cities:${e}`) : ""),
       id: e,
     }));
 
   const formatOptions = (options: string[]) => {
-    return options.map((e:string)=>(
-      {
-        label: t(`common:${e}`),
-        id: e
-      }
-    ))
-  }
+    return options.map((e: string) => ({
+      label: t(`common:${e}`),
+      id: e,
+    }));
+  };
 
   const handleSearchClick = () => setCityFilter(city);
 
-  const handleAutocompleteChange = (value: string) => value !== "any_city" ? setCity(value) : setCity("");
+  const handleAutocompleteChange = (value: string) =>
+    value !== "any_city" ? setCity(value) : setCity("");
 
   const handleDownloadFile = () =>
-    downloadFile(JSON.stringify(properties), createFileName(typeFilter), "text/plain", downloadLinkRef);
-  
-  
+    downloadFile(
+      JSON.stringify(properties),
+      createFileName(typeFilter),
+      "text/plain",
+      downloadLinkRef
+    );
+
   if (isContextReady !== true) {
     return <p>CARGANDO</p>;
   }
-
 
   return (
     <div className={styles.container}>
@@ -90,16 +89,21 @@ const Home: NextPage = () => {
         <section className={styles.filters_container}>
           <div className={styles.filter_container}>
             <AutoCompleteInput
+              width={"80%"}
               label={t("common:choose_a_city")}
-              defaultValue={{label: t(`cities:${cityFilter}`), id: cityFilter}}
+              defaultValue={{
+                label: t(`cities:${cityFilter}`),
+                id: cityFilter,
+              }}
               noOptionsText={"No hay resultados para mostrar"}
               options={translateCities(cityOptions)}
               onChange={handleAutocompleteChange}
             />
-            <IconButton icon={"search"} onClick={handleSearchClick} light/>
+            <IconButton icon={"search"} onClick={handleSearchClick} light />
           </div>
           <div className={styles.filter_container}>
             <SelectInput
+              width={"100%"}
               label={t("common:order")}
               value={priceOrder}
               options={[
@@ -109,37 +113,36 @@ const Home: NextPage = () => {
               onChange={setPriceOrder}
             />
           </div>
-          <div className={styles.filter_container}>
-            <CheckboxInput 
-              title={t("common:filter")}
-              options={formatOptions(propertiesTypesOptions)}
-              selected={typeFilter}
-              onChange={setTypeFilter}
-            />
-          </div>
-          <div className={styles.filter_container}>
-            <StandardButton 
-              text={t("common:download_list")} 
-              onClick={handleDownloadFile}>
-              <a
-                ref={downloadLinkRef}
+          <div className={styles.checkbox_button_container}>
+            <div className={styles.checkbox_container}>
+              <CheckboxInput
+                title={t("common:filter")}
+                options={formatOptions(propertiesTypesOptions)}
+                selected={typeFilter}
+                onChange={setTypeFilter}
               />
-            </StandardButton>
+            </div>
+            <div className={styles.button_container}>
+              <StandardButton
+                text={t("common:download_list")}
+                onClick={handleDownloadFile}
+              >
+                <a ref={downloadLinkRef} />
+              </StandardButton>
+            </div>
           </div>
         </section>
         <section className={styles.cards_container}>
           <article className={styles.pagination_container}>
             <Pagination
-              totalPages={Math.ceil(totalProperties/itemsPerPage)}
-              activePage={page+1}
+              totalPages={Math.ceil(totalProperties / itemsPerPage)}
+              activePage={page + 1}
               onChange={setPage}
             />
           </article>
           <CardList list={properties} />
         </section>
       </main>
-
-      <Footer />
     </div>
   );
 };
